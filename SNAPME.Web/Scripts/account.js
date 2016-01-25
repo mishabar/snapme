8c225df0f2872ec3df7a8348983a2cad31fed5ae
Michael Bar <barmic@gmail.com>
2016-01-25 17:15:42 -0200

    System Messages and Friends Activity UI

--- SNAPME.Web/Scripts/account.js
+++ SNAPME.Web/Scripts/account.js
@@ -1,6 +1,10 @@
 ﻿// create angular controller
 iisnapApp.controller('accountController', function ($scope, $http, $cookies, sharedDataService) {
 
+    $scope.friends_activities = [{ "user_id": "56735468b207ab4148aba379", "user_name": "Michael Bar", "type": 3, "text": "Michael Bar joined the sale", "created_on": "2 weeks ago", "data": { "price": 16418 } }];
+    $scope.hasActivities = true;
+    $scope.system_messages = [];
+
     $scope.init = function (name) {
         sharedDataService.set('user', { name: name });
     }
