5fb83e03036065623485031bb738f347eddd11fc
Michael Bar <barmic@gmail.com>
2016-03-29 11:56:37 -0300

    another try

--- SNAPME.Live/Global.asax.cs
+++ SNAPME.Live/Global.asax.cs
@@ -25,7 +25,7 @@ namespace SNAPME.Live
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
