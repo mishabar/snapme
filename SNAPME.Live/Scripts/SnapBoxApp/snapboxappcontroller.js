1a9a4f0860c549b5922ad165f625383655dd73a9
Michael Bar <barmic@gmail.com>
2016-05-03 17:56:30 -0300

    Final touches

--- SNAPME.Live/Scripts/SnapBoxApp/snapboxappcontroller.js
+++ SNAPME.Live/Scripts/SnapBoxApp/snapboxappcontroller.js
@@ -1,14 +1,14 @@
 ﻿app.controller('SnapBoxAppController', function ($scope, $filter, $mdSidenav) {
     $scope.products = [
-        { id: 1, name: 'ASUS VM42-S075V Desktop', image: 'http://ecx.images-amazon.com/images/I/41QmEpAHnLL.jpg', want: true, wants: 69, supplier: { name: 'ASUS', image: '/content/images/snapapp/asus.png', category: 'Tech' } },
-        { id: 2, name: 'ICEORB', image: 'http://ecx.images-amazon.com/images/I/51dOi4Mr6wL.jpg', want: false, wants: 123, supplier: { name: 'ICE', image: '/content/images/snapapp/ice.jpg', category: 'Tech' } },
-        { id: 3, name: '', image: 'http://dy6g3i6a1660s.cloudfront.net/Oms82f5Ssy9smF0Ued1uTwR-BH4/zb_p.jpg', want: false, wants: 77, supplier: { name: 'SEPHORA', image: '/content/images/snapapp/sephora.gif', category: 'BEAUTY' } }
+        { id: 1, name: 'ASUS VM42-S075V Desktop', image: 'http://ecx.images-amazon.com/images/I/41QmEpAHnLL.jpg', want: true, wants: 69, category: 'Tech', supplier: { name: 'ASUS', image: '/content/images/snapapp/asus.png', category: 'Tech' } },
+        { id: 2, name: 'ICEORB', image: 'http://ecx.images-amazon.com/images/I/51dOi4Mr6wL.jpg', want: false, wants: 123, category: 'Tech', supplier: { name: 'ICE', image: '/content/images/snapapp/ice.jpg', category: 'Tech' } },
+        { id: 3, name: 'Brow Powder Duo', image: 'http://dy6g3i6a1660s.cloudfront.net/Oms82f5Ssy9smF0Ued1uTwR-BH4/zb_p.jpg', want: false, wants: 77, category: 'Beauty', supplier: { name: 'SEPHORA', image: '/content/images/snapapp/sephora.gif', category: 'Beauty' } }
     ];
     $scope.alerts = [];
     $scope.mybox = $filter('filter')($scope.products, { want: true });
 
-    $scope.categories = ['Beauty', 'Tech'];
-    $scope.selectedCategory = 'Beauty';
+    $scope.categories = [{ id: 1, name: 'Beauty' }, { id: 2, name: 'Tech' }];
+    $scope.selectedCategory = 0;
 
     $scope.toggleSidenav = function () {
         $mdSidenav('sideNav').toggle();
@@ -16,6 +16,14 @@
 
     $scope.encodeUriComponent = function (value) {
         return encodeURIComponent(value);
+    }
+
+    $scope.filteredProducts = $filter('filter')($scope.products, { category: $scope.categories[$scope.selectedCategory].name });
+
+    $scope.selectCategory = function (idx) {
+        $scope.selectedCategory = idx;
+        $scope.filteredProducts = $filter('filter')($scope.products, { category: $scope.categories[$scope.selectedCategory].name });
+        $scope.toggleSidenav();
     }
 
     $scope.toggleBox = function (product) {
