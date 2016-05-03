081b540625bcbeb50468b5539b9245a7ffeebc23
Michael Bar <barmic@gmail.com>
2016-05-03 17:41:37 -0300

    More app things

--- SNAPME.Live/Scripts/SnapBoxApp/snapboxappcontroller.js
+++ SNAPME.Live/Scripts/SnapBoxApp/snapboxappcontroller.js
@@ -1,7 +1,33 @@
-﻿app.controller('SnapBoxAppController', function ($scope) {
+﻿app.controller('SnapBoxAppController', function ($scope, $filter, $mdSidenav) {
     $scope.products = [
-        { id: 1, name: 'ASUS VM42-S075V Desktop', image: 'http://ecx.images-amazon.com/images/I/41QmEpAHnLL.jpg', likes: 69, supplier: { name: 'Michael Bar', image: '//graph.facebook.com/10152734377022123/picture?type=square' } },
-        { id: 2, name: 'ICEORB', image: 'http://ecx.images-amazon.com/images/I/51dOi4Mr6wL.jpg', likes: 123, supplier: { name: 'Ami Barda', image: '//graph.facebook.com/10152686035091417/picture?type=square' } }
+        { id: 1, name: 'ASUS VM42-S075V Desktop', image: 'http://ecx.images-amazon.com/images/I/41QmEpAHnLL.jpg', want: true, wants: 69, supplier: { name: 'ASUS', image: '/content/images/snapapp/asus.png', category: 'Tech' } },
+        { id: 2, name: 'ICEORB', image: 'http://ecx.images-amazon.com/images/I/51dOi4Mr6wL.jpg', want: false, wants: 123, supplier: { name: 'ICE', image: '/content/images/snapapp/ice.jpg', category: 'Tech' } },
+        { id: 3, name: '', image: 'http://dy6g3i6a1660s.cloudfront.net/Oms82f5Ssy9smF0Ued1uTwR-BH4/zb_p.jpg', want: false, wants: 77, supplier: { name: 'SEPHORA', image: '/content/images/snapapp/sephora.gif', category: 'BEAUTY' } }
     ];
-    $scope.mybox = [];
+    $scope.alerts = [];
+    $scope.mybox = $filter('filter')($scope.products, { want: true });
+
+    $scope.categories = ['Beauty', 'Tech'];
+    $scope.selectedCategory = 'Beauty';
+
+    $scope.toggleSidenav = function () {
+        $mdSidenav('sideNav').toggle();
+    }
+
+    $scope.encodeUriComponent = function (value) {
+        return encodeURIComponent(value);
+    }
+
+    $scope.toggleBox = function (product) {
+        var idx = $scope.mybox.indexOf(product);
+        if (idx == -1) {
+            $scope.mybox.push(product);
+            product.want = true;
+            product.wants++;
+        } else {
+            $scope.mybox.splice(idx, 1);
+            product.want = false;
+            product.wants--;
+        }
+    }
 });
