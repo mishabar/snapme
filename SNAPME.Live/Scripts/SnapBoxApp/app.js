2cf7d814344ee84ec9d86ee8b0ed3cf4bd8d1111
Michael Bar <barmic@gmail.com>
2016-05-03 20:17:54 -0300

    allowing intent in href

--- SNAPME.Live/Scripts/SnapBoxApp/app.js
+++ SNAPME.Live/Scripts/SnapBoxApp/app.js
@@ -1,6 +1,7 @@
 ﻿var app = angular.module('IISNAPBOXAPP', ['ngMessages', 'ngMaterial', 'ngSanitize', 'ngCookies', 'ui.validate']);
-app.config(function ($mdThemingProvider) {
+app.config(function ($mdThemingProvider, $compileProvider) {
     $mdThemingProvider.theme('default')
         .primaryPalette('grey')
-        .accentPalette('amber')
+        .accentPalette('amber');
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|intent):/);
 });
